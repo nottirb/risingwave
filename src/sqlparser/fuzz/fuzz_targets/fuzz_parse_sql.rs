b7b76d24e9087191f53b93b6a4d03b57b007d8fb
britton <brittonkfischer@gmail.com>
2022-05-15 22:10:29 -0700

    Update Fuzz Testing

--- src/sqlparser/fuzz/fuzz_targets/fuzz_parse_sql.rs
+++ src/sqlparser/fuzz/fuzz_targets/fuzz_parse_sql.rs
@@ -13,14 +13,12 @@
 // limitations under the License.
 
 use honggfuzz::fuzz;
-use risingwave_sqlparser::dialect::GenericDialect;
 use risingwave_sqlparser::parser::Parser;
 
 fn main() {
     loop {
         fuzz!(|data: String| {
-            let dialect = GenericDialect {};
-            let _ = Parser::parse_sql(&dialect, &data);
+            let _ = Parser::parse_sql(&data);
         });
     }
 }
